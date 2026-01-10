ee0be8dec99aeb8ca59190e71c706b4ca2746a8b
smudg <68889544+SidharthMudgil@users.noreply.github.com>
2026-01-10 19:39:41 +0530

    fix compilation issue

--- feature/movie-details/src/main/java/com/sidharth/movie_details/domain/usecase/GetMovieDetailsUseCase.kt
+++ feature/movie-details/src/main/java/com/sidharth/movie_details/domain/usecase/GetMovieDetailsUseCase.kt
@@ -1,14 +1,14 @@
 package com.sidharth.movie_details.domain.usecase
 
+import com.sidharth.model.Movie
 import com.sidharth.model.ResultState
-import com.sidharth.model.SearchItem
-import com.sidharth.search.domain.repository.SearchRepository
+import com.sidharth.movie_details.domain.repository.MovieRepository
 import kotlinx.coroutines.flow.Flow
 
-internal class GetMovieDetailsUseCase (
-    private val searchRepository: SearchRepository
+internal class GetMovieDetailsUseCase(
+    private val movieRepository: MovieRepository
 ) {
-    suspend operator fun invoke(query: String): Flow<ResultState<List<SearchItem>>> {
-        return searchRepository.searchMovie(query)
+    suspend operator fun invoke(movieId: Int): Flow<ResultState<Movie>> {
+        return movieRepository.getMovieDetails(movieId)
     }
 }
